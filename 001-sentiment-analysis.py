f29bfd5b47b1073a04f1d60baaee05693220b929
Mihir Raj Chowdhury <rajmihir945@gmail.com>
2025-04-28 00:55:20 +0530

    used chatGroq along with practicing

--- 001-sentiment-analysis.py
+++ 001-sentiment-analysis.py
@@ -1,46 +1,161 @@
-import os
+# import os
+# from dotenv import load_dotenv, find_dotenv
+# _ = load_dotenv(find_dotenv())
+# # openai_api_key = os.environ["OPENAI_API_KEY"]
+
+# # from langchain_openai import ChatOpenAI
+
+# # llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+
+# from langchain_groq import ChatGroq
+
+# groq_api_key = os.environ["GROQ_API_KEY"] 
+
+# llm = ChatGroq(model_name="llama-3.1-8b-instant", groq_api_key=groq_api_key)
+
+# from langchain_core.prompts import ChatPromptTemplate
+# from pydantic import BaseModel, Field
+
+# tagging_prompt = ChatPromptTemplate.from_template(
+#     """
+# Extract the desired information from the following passage.
+
+# Only extract the properties mentioned in the 'Classification' function.
+
+# Passage:
+# {input}
+# """
+# )
+
+# class Classification(BaseModel):
+#     sentiment: str = Field(description="The sentiment of the text")
+#     political_tendency: str = Field(
+#         description="The political tendency of the user"
+#     )
+#     language: str = Field(description="The language the text is written in")
+
+
+# # LLM
+# llm = ChatGroq(temperature=0, model_name="llama-3.1-8b-instant").with_structured_output(
+#     Classification
+# )
+
+# tagging_chain = tagging_prompt | llm
+
+# trump_follower = "I'm confident that President Trump's leadership and track record will once again resonate with Americans. His strong stance on economic growth and national security is exactly what our country needs at this pivotal moment. We need to bring back the proven leadership that can make America great again!"
+
+# biden_follower = "I believe President Biden's compassionate and steady approach is vital for our nation right now. His commitment to healthcare reform, climate change, and restoring our international alliances is crucial. It's time to continue the progress and ensure a future that benefits all Americans."
+
+# response = tagging_chain.invoke({"input": trump_follower})
+
+# print("\n----------\n")
+
+# print("Sentiment analysis Trump follower:")
+
+# print("\n----------\n")
+# print(response)
+
+# print("\n----------\n")
+
+# response = tagging_chain.invoke({"input": biden_follower})
+
+# print("\n----------\n")
+
+# print("Sentiment analysis Biden follower:")
+
+# print("\n----------\n")
+# print(response)
+
+# print("\n----------\n")
+
+# class Classification(BaseModel):
+#     sentiment: str = Field(..., enum=["happy", "neutral", "sad"])
+#     political_tendency: str = Field(
+#         ...,
+#         description="The political tendency of the user",
+#         enum=["conservative", "liberal", "independent"],
+#     )
+#     language: str = Field(
+#         ..., enum=["spanish", "english"]
+#     )
+    
+# tagging_prompt = ChatPromptTemplate.from_template(
+#     """
+# Extract the desired information from the following passage.
+
+# Only extract the properties mentioned in the 'Classification' function.
+
+# Passage:
+# {input}
+# """
+# )
+
+# llm = ChatGroq(temperature=0, model_name="llama-3.1-8b-instant").with_structured_output(
+#     Classification
+# )
+
+# tagging_chain = tagging_prompt | llm
+
+# response = tagging_chain.invoke({"input": trump_follower})
+
+# print("\n----------\n")
+
+# print("Sentiment analysis Trump follower (with a list of options using enums):")
+
+# print("\n----------\n")
+# print(response)
+
+# print("\n----------\n")
+
+# response = tagging_chain.invoke({"input": biden_follower})
+
+# print("\n----------\n")
+
+# print("Sentiment analysis Biden follower (with a list of options using enums):")
+
+# print("\n----------\n")
+# print(response)
+
+# print("\n----------\n")
+
+
+import os 
 from dotenv import load_dotenv, find_dotenv
+
 _ = load_dotenv(find_dotenv())
-openai_api_key = os.environ["OPENAI_API_KEY"]
 
-from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+groq_api_key = os.environ["GROQ_API_KEY"]
+llm = ChatGroq(model="llama-3.1-8b-instant",groq_api_key = groq_api_key)
 
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 
 tagging_prompt = ChatPromptTemplate.from_template(
+    """Extract the desired information from the following passage.
+    
+    Only extract the properties mentioned in the 'Classification' function.
+
+    Passage
+    {input}
     """
-Extract the desired information from the following passage.
-
-Only extract the properties mentioned in the 'Classification' function.
-
-Passage:
-{input}
-"""
-)
+    )
 
 class Classification(BaseModel):
-    sentiment: str = Field(description="The sentiment of the text")
-    political_tendency: str = Field(
-        description="The political tendency of the user"
-    )
-    language: str = Field(description="The language the text is written in")
+    sentiment:str = Field(description="The sentiment of the audience",enum=["hard","soft","enjoy"])
+    political_tendency:str = Field(description="The political tendency of the user",enum=["With trump","With Biden","With No one"])
+    language:str = Field(description="The language text is written in ",enum=["Hindi","Urdu"])
 
-
-# LLM
-llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0125").with_structured_output(
-    Classification
-)
-
+llm = ChatGroq(temperature=0,model="llama-3.1-8b-instant").with_structured_output(schema=Classification)
 tagging_chain = tagging_prompt | llm
+
 
 trump_follower = "I'm confident that President Trump's leadership and track record will once again resonate with Americans. His strong stance on economic growth and national security is exactly what our country needs at this pivotal moment. We need to bring back the proven leadership that can make America great again!"
 
 biden_follower = "I believe President Biden's compassionate and steady approach is vital for our nation right now. His commitment to healthcare reform, climate change, and restoring our international alliances is crucial. It's time to continue the progress and ensure a future that benefits all Americans."
 
-response = tagging_chain.invoke({"input": trump_follower})
+response = tagging_chain.invoke({"input":trump_follower})
 
 print("\n----------\n")
 
@@ -51,61 +166,11 @@ print(response)
 
 print("\n----------\n")
 
-response = tagging_chain.invoke({"input": biden_follower})
+response = tagging_chain.invoke({"input":biden_follower})
 
 print("\n----------\n")
 
 print("Sentiment analysis Biden follower:")
-
-print("\n----------\n")
-print(response)
-
-print("\n----------\n")
-
-class Classification(BaseModel):
-    sentiment: str = Field(..., enum=["happy", "neutral", "sad"])
-    political_tendency: str = Field(
-        ...,
-        description="The political tendency of the user",
-        enum=["conservative", "liberal", "independent"],
-    )
-    language: str = Field(
-        ..., enum=["spanish", "english"]
-    )
-    
-tagging_prompt = ChatPromptTemplate.from_template(
-    """
-Extract the desired information from the following passage.
-
-Only extract the properties mentioned in the 'Classification' function.
-
-Passage:
-{input}
-"""
-)
-
-llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0125").with_structured_output(
-    Classification
-)
-
-tagging_chain = tagging_prompt | llm
-
-response = tagging_chain.invoke({"input": trump_follower})
-
-print("\n----------\n")
-
-print("Sentiment analysis Trump follower (with a list of options using enums):")
-
-print("\n----------\n")
-print(response)
-
-print("\n----------\n")
-
-response = tagging_chain.invoke({"input": biden_follower})
-
-print("\n----------\n")
-
-print("Sentiment analysis Biden follower (with a list of options using enums):")
 
 print("\n----------\n")
 print(response)
